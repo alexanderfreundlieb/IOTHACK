b4f99216119dac804d990f0d279e9f792522caa5
Alexander Freundlieb <unset>
2026-09-08 15:23:37 +0200

    help

--- hackathon/hardhat.config.ts
+++ hackathon/hardhat.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
-    version: "0.8.34",
+    profiles: {
+      default: {
+        version: "0.8.34",
+      },
+      production: {
+        version: "0.8.34",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      chainType: "l1",
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")], // DEPLOY PRIVATE KEY
+    },
   },
 });
